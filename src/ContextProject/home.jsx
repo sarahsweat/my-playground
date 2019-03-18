36f88b511d640bdb0dfdd5928176fd3e188bf5da
Sarah Sweat <sarahsweat@gmail.com>
2019-03-18 08:58:29 -0400

    create initial storybook and context proj

---
+++ src/ContextProject/home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const Home = () => (
+  <div style={{
+      height: '200px',
+      width: '400px',
+      textAlign: 'center',
+      backgroundColor: 'yellow'
+    }}
+  >
+    <h1>
+      This is my Home Page Component
+    </h1>
+  </div>
+)
---
+++ src/ContextProject/home.stories.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+import { Home } from './home'
+
+storiesOf('Context API Project', module)
+.add('Home', () => <Home />);
